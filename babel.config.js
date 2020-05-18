dbb7884d7e3967e567097089a2c04893f7367ea0
DaniAkash <s.daniakash@gmail.com>
2020-05-19 05:02:40 +0530

    created project

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = (api) => {
+  const isTestEnv = api.env("test");
+
+  return {
+    babelrc: false,
+    ignore: ["./node_modules"],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          loose: true,
+          targets: isTestEnv
+            ? { node: "current" }
+            : { browserslist: "> 0.25%, not dead" },
+        },
+      ],
+      "@babel/preset-typescript",
+    ],
+  };
+};
